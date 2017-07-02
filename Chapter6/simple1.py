ceff95db10bca9680f248e1edc2e5d78c0ead82e
MiracleWong <cfwr1991@126.com>
2017-07-02 11:52:32 +0800

    add the paramiko sample1

---
+++ Chapter6/simple1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import paramiko
+
+hostname = '192.168.125.126'
+username = 'root'
+password = '123456'
+
+# 发送paramiko日志到syslogin.log文件
+paramiko.util.log_to_file('syslogin.log')
+
+# 创建一个SSHClient对象
+ssh = paramiko.SSHClient()
+ssh.load_system_host_keys()
+
+ssh.connect(hostname=hostname,username=username,password=password)
+stdin, stdout, stderr = ssh.exec_command('free -m')
+print stdout.read()
+ssh.close()
