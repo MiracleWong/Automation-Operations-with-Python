c3555221bacd4feb71682de00f4f2ee759fdb4a7
MiracleWong <cfwr1991@126.com>
2017-07-29 17:42:24 +0800

    ftp the files by paramiko

--- Chapter6/sftp.py
+++ Chapter6/sftp.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import paramiko
+import sys
+reload(sys) 
+sys.setdefaultencoding("utf-8")
 
 hostname = '192.168.125.126'
 username = 'root'
@@ -8,16 +11,18 @@ password = '123456'
 port=22
 
 try:
-    
+    t = paramiko.Transport((hostname, port))
+    t.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(t)
+
+    sftp.put("hello.txt", "/root/hello.txt") #上传文件
+    sftp.get("/root/hello.txt", "hello1.txt") #下载文件
+    sftp.mkdir("/root/userdir",0755) #创建目录
+    print "Create successgfully"
+    sftp.rmdir("/root/userdir") # 删除目录
+    sftp.rename("/root/testfile.sh","/root/test.sh") # 文件重命名
+    print sftp.stat("/root/test.sh") # 打印文件信息
+    print sftp.listdir("/root") # 打印目录列表
+    t.close()
 except Exception, e:
     print str(e)
-# 创建一个SSHClient对象
-ssh = paramiko.SSHClient()
-ssh.load_system_host_keys()
-privateKey = os.path.expanduser('~/.ssh/id_rsa')  #自定义私钥的存放路径
-key = paramiko.RSAKey.from_private_key_file(privateKey) #创建私钥对象key
-
-ssh.connect(hostname=hostname,username=username,pkey=key)
-stdin, stdout, stderr = ssh.exec_command('free -m')
-print stdout.read()
-ssh.close()
